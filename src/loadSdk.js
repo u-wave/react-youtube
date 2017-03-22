8c29787e5e1bb22129c80644b92d6927914d7b73
René Kooi <renee@kooi.me>
2017-03-22 12:53:30 +0100

    Simplify `loadSdk`.

--- src/loadSdk.js
+++ src/loadSdk.js
@@ -3,27 +3,21 @@ import loadScript from 'load-script';
 
 function loadSdk() {
   return new Promise((resolve, reject) => {
-    if (typeof window.YT === 'object' && typeof window.YT.Player === 'function') {
+    if (typeof window.YT === 'object' && typeof window.YT.ready === 'function') {
       // A YouTube SDK is already loaded, so reuse that
-      resolve(window.YT);
+      window.YT.ready(() => {
+        resolve(window.YT);
+      });
       return;
     }
-
-    const onYouTubeIframeAPIReady = window.onYouTubeIframeAPIReady;
-    window.onYouTubeIframeAPIReady = () => {
-      if (onYouTubeIframeAPIReady) {
-        onYouTubeIframeAPIReady();
-      }
-      // Revert.
-      window.onYouTubeIframeAPIReady = onYouTubeIframeAPIReady;
-
-      resolve(window.YT);
-    };
 
     loadScript('https://www.youtube.com/iframe_api', (err) => {
       if (err) {
         reject(err);
       }
+      window.YT.ready(() => {
+        resolve(window.YT);
+      });
     });
   });
 }
