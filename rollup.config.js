1062e4770d171e869ddda6bd3213c744a56d2287
René Kooi <renee@kooi.me>
2017-04-10 12:58:50 +0200

    Use babel-preset-env.

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,8 @@
-import fs from 'fs';
-import path from 'path';
 import babel from 'rollup-plugin-babel';
 
 const meta = require('./package.json');
 
-// build a babel config that works for the rollup babel plugin
-function getBabelConfig() {
-  const babelrc = fs.readFileSync(path.join(__dirname, '.babelrc'), 'utf8');
-  const babelConfig = JSON.parse(babelrc);
-  babelConfig.presets[0] = ['es2015', { loose: true, modules: false }];
-  babelConfig.plugins.push('external-helpers');
-  babelConfig.babelrc = false;
-  return babelConfig;
-}
+process.env.BABEL_ENV = 'rollup';
 
 export default {
   entry: './src/index.js',
@@ -24,6 +14,6 @@ export default {
   external: Object.keys(meta.dependencies)
     .concat(Object.keys(meta.peerDependencies)),
   plugins: [
-    babel(getBabelConfig()),
+    babel(),
   ],
 };
