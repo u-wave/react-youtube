5dcb0604af84a310b03c5c4b85a4af36c4f176ad
Renée Kooi <renee@kooi.me>
2017-10-01 18:44:21 +0200

    rm remove-prop-types transform

--- src/index.js
+++ src/index.js
@@ -4,201 +4,6 @@ import eventNames from './eventNames';
 import loadSdk from './loadSdk';
 
 class YouTube extends React.Component {
-  static propTypes = {
-    /**
-     * An 11-character string representing a YouTube video ID..
-     */
-    video: PropTypes.string,
-    /**
-     * DOM ID for the player element.
-     */
-    id: PropTypes.string,
-    /**
-     * CSS className for the player element.
-     */
-    className: PropTypes.string,
-    /**
-     * Width of the player element.
-     */
-    width: PropTypes.oneOfType([
-      PropTypes.number,
-      PropTypes.string,
-    ]),
-    /**
-     * Height of the player element.
-     */
-    height: PropTypes.oneOfType([
-      PropTypes.number,
-      PropTypes.string,
-    ]),
-
-    /**
-     * Pause the video.
-     */
-    paused: PropTypes.bool, // eslint-disable-line react/no-unused-prop-types
-
-    // Player parameters
-
-    /**
-     * Whether the video should start playing automatically.
-     *
-     * https://developers.google.com/youtube/player_parameters#autoplay
-     */
-    autoplay: PropTypes.bool,
-    /**
-     * Whether to show captions below the video.
-     *
-     * https://developers.google.com/youtube/player_parameters#cc_load_policy
-     */
-    showCaptions: PropTypes.bool,
-    /**
-     * Whether to show video controls.
-     *
-     * https://developers.google.com/youtube/player_parameters#controls
-     */
-    controls: PropTypes.bool,
-    /**
-     * Ignore keyboard controls.
-     *
-     * https://developers.google.com/youtube/player_parameters#disablekb
-     */
-    disableKeyboard: PropTypes.bool,
-    /**
-     * Whether to display the fullscreen button.
-     *
-     * https://developers.google.com/youtube/player_parameters#fs
-     */
-    allowFullscreen: PropTypes.bool,
-    /**
-     * The player's interface language. The parameter value is an ISO 639-1
-     * two-letter language code or a fully specified locale.
-     *
-     * https://developers.google.com/youtube/player_parameters#hl
-     */
-    lang: PropTypes.string,
-    /**
-     * Whether to show annotations on top of the video.
-     *
-     * https://developers.google.com/youtube/player_parameters#iv_load_policy
-     */
-    annotations: PropTypes.bool,
-    /**
-     * Time in seconds at which to start playing the video.
-     *
-     * https://developers.google.com/youtube/player_parameters#start
-     */
-    startSeconds: PropTypes.number,
-    /**
-     * Time in seconds at which to stop playing the video.
-     *
-     * https://developers.google.com/youtube/player_parameters#end
-     */
-    endSeconds: PropTypes.number,
-    /**
-     * Remove most YouTube logos from the player.
-     *
-     * https://developers.google.com/youtube/player_parameters#modestbranding
-     */
-    modestBranding: PropTypes.bool,
-    /**
-     * Whether to play the video inline on iOS, instead of fullscreen.
-     *
-     * https://developers.google.com/youtube/player_parameters#playsinline
-     */
-    playsInline: PropTypes.bool,
-    /**
-     * Whether to show related videos after the video is over.
-     *
-     * https://developers.google.com/youtube/player_parameters#rel
-     */
-    showRelatedVideos: PropTypes.bool,
-    /**
-     * Whether to show video information (uploader, title, etc) before the video
-     * starts.
-     *
-     * https://developers.google.com/youtube/player_parameters#showinfo
-     */
-    showInfo: PropTypes.bool,
-
-    /**
-     * The playback volume, **as a number between 0 and 1**.
-     */
-    volume: PropTypes.number,
-
-    /**
-     * Whether the video's sound should be muted.
-     */
-    muted: PropTypes.bool,
-
-    /**
-     * The suggested playback quality.
-     *
-     * https://developers.google.com/youtube/iframe_api_reference#Playback_quality
-     */
-    suggestedQuality: PropTypes.string,
-    /**
-     * Playback speed.
-     *
-     * https://developers.google.com/youtube/iframe_api_reference#setPlaybackRate
-     */
-    playbackRate: PropTypes.number,
-
-    // Events
-    /* eslint-disable react/no-unused-prop-types */
-
-    /**
-     * Sent when the YouTube player API has loaded.
-     */
-    onReady: PropTypes.func,
-    /**
-     * Sent when the player triggers an error.
-     */
-    onError: PropTypes.func,
-    /**
-     * Sent when the video is cued and ready to play.
-     */
-    onCued: PropTypes.func,
-    /**
-     * Sent when the video is buffering.
-     */
-    onBuffering: PropTypes.func,
-    /**
-     * Sent when playback has been started or resumed.
-     */
-    onPlaying: PropTypes.func,
-    /**
-     * Sent when playback has been paused.
-     */
-    onPause: PropTypes.func,
-    /**
-     * Sent when playback has stopped.
-     */
-    onEnd: PropTypes.func,
-    onStateChange: PropTypes.func,
-    onPlaybackRateChange: PropTypes.func,
-    onPlaybackQualityChange: PropTypes.func,
-
-    /* eslint-enable react/no-unused-prop-types */
-  };
-
-  static defaultProps = {
-    autoplay: false,
-    showCaptions: false,
-    controls: true,
-    disableKeyboard: false,
-    allowFullscreen: true,
-    annotations: true,
-    modestBranding: false,
-    playsInline: false,
-    showRelatedVideos: true,
-    showInfo: true,
-    onCued: () => {},
-    onBuffering: () => {},
-    onPlaying: () => {},
-    onPause: () => {},
-    onEnd: () => {},
-  };
-
   componentDidMount() {
     this.createPlayer();
   }
@@ -395,5 +200,202 @@ class YouTube extends React.Component {
     );
   }
 }
+
+if (process.env.NODE_ENV !== 'production') {
+  YouTube.propTypes = {
+    /**
+     * An 11-character string representing a YouTube video ID..
+     */
+    video: PropTypes.string,
+    /**
+     * DOM ID for the player element.
+     */
+    id: PropTypes.string,
+    /**
+     * CSS className for the player element.
+     */
+    className: PropTypes.string,
+    /**
+     * Width of the player element.
+     */
+    width: PropTypes.oneOfType([
+      PropTypes.number,
+      PropTypes.string,
+    ]),
+    /**
+     * Height of the player element.
+     */
+    height: PropTypes.oneOfType([
+      PropTypes.number,
+      PropTypes.string,
+    ]),
+
+    /**
+     * Pause the video.
+     */
+    paused: PropTypes.bool, // eslint-disable-line react/no-unused-prop-types
+
+    // Player parameters
+
+    /**
+     * Whether the video should start playing automatically.
+     *
+     * https://developers.google.com/youtube/player_parameters#autoplay
+     */
+    autoplay: PropTypes.bool,
+    /**
+     * Whether to show captions below the video.
+     *
+     * https://developers.google.com/youtube/player_parameters#cc_load_policy
+     */
+    showCaptions: PropTypes.bool,
+    /**
+     * Whether to show video controls.
+     *
+     * https://developers.google.com/youtube/player_parameters#controls
+     */
+    controls: PropTypes.bool,
+    /**
+     * Ignore keyboard controls.
+     *
+     * https://developers.google.com/youtube/player_parameters#disablekb
+     */
+    disableKeyboard: PropTypes.bool,
+    /**
+     * Whether to display the fullscreen button.
+     *
+     * https://developers.google.com/youtube/player_parameters#fs
+     */
+    allowFullscreen: PropTypes.bool,
+    /**
+     * The player's interface language. The parameter value is an ISO 639-1
+     * two-letter language code or a fully specified locale.
+     *
+     * https://developers.google.com/youtube/player_parameters#hl
+     */
+    lang: PropTypes.string,
+    /**
+     * Whether to show annotations on top of the video.
+     *
+     * https://developers.google.com/youtube/player_parameters#iv_load_policy
+     */
+    annotations: PropTypes.bool,
+    /**
+     * Time in seconds at which to start playing the video.
+     *
+     * https://developers.google.com/youtube/player_parameters#start
+     */
+    startSeconds: PropTypes.number,
+    /**
+     * Time in seconds at which to stop playing the video.
+     *
+     * https://developers.google.com/youtube/player_parameters#end
+     */
+    endSeconds: PropTypes.number,
+    /**
+     * Remove most YouTube logos from the player.
+     *
+     * https://developers.google.com/youtube/player_parameters#modestbranding
+     */
+    modestBranding: PropTypes.bool,
+    /**
+     * Whether to play the video inline on iOS, instead of fullscreen.
+     *
+     * https://developers.google.com/youtube/player_parameters#playsinline
+     */
+    playsInline: PropTypes.bool,
+    /**
+     * Whether to show related videos after the video is over.
+     *
+     * https://developers.google.com/youtube/player_parameters#rel
+     */
+    showRelatedVideos: PropTypes.bool,
+    /**
+     * Whether to show video information (uploader, title, etc) before the video
+     * starts.
+     *
+     * https://developers.google.com/youtube/player_parameters#showinfo
+     */
+    showInfo: PropTypes.bool,
+
+    /**
+     * The playback volume, **as a number between 0 and 1**.
+     */
+    volume: PropTypes.number,
+
+    /**
+     * Whether the video's sound should be muted.
+     */
+    muted: PropTypes.bool,
+
+    /**
+     * The suggested playback quality.
+     *
+     * https://developers.google.com/youtube/iframe_api_reference#Playback_quality
+     */
+    suggestedQuality: PropTypes.string,
+    /**
+     * Playback speed.
+     *
+     * https://developers.google.com/youtube/iframe_api_reference#setPlaybackRate
+     */
+    playbackRate: PropTypes.number,
+
+    // Events
+    /* eslint-disable react/no-unused-prop-types */
+
+    /**
+     * Sent when the YouTube player API has loaded.
+     */
+    onReady: PropTypes.func,
+    /**
+     * Sent when the player triggers an error.
+     */
+    onError: PropTypes.func,
+    /**
+     * Sent when the video is cued and ready to play.
+     */
+    onCued: PropTypes.func,
+    /**
+     * Sent when the video is buffering.
+     */
+    onBuffering: PropTypes.func,
+    /**
+     * Sent when playback has been started or resumed.
+     */
+    onPlaying: PropTypes.func,
+    /**
+     * Sent when playback has been paused.
+     */
+    onPause: PropTypes.func,
+    /**
+     * Sent when playback has stopped.
+     */
+    onEnd: PropTypes.func,
+    onStateChange: PropTypes.func,
+    onPlaybackRateChange: PropTypes.func,
+    onPlaybackQualityChange: PropTypes.func,
+
+    /* eslint-enable react/no-unused-prop-types */
+  };
+}
+
+YouTube.defaultProps = {
+  autoplay: false,
+  showCaptions: false,
+  controls: true,
+  disableKeyboard: false,
+  allowFullscreen: true,
+  annotations: true,
+  modestBranding: false,
+  playsInline: false,
+  showRelatedVideos: true,
+  showInfo: true,
+  onCued: () => {},
+  onBuffering: () => {},
+  onPlaying: () => {},
+  onPause: () => {},
+  onEnd: () => {},
+};
 
 export default YouTube;
