16d46fe5181207102d3e0120c163f0ec82a4b11a
Renée Kooi <renee@kooi.me>
2018-09-06 22:41:47 +0200

    Test fixes for react 16.5 (#56)
    * chore(package): update react to version 16.5.0
    
    * chore(package): update react-dom to version 16.5.0
    
    * chore(package): update react-test-renderer to version 16.5.0
    
    * Fixes

--- test/util/render.js
+++ test/util/render.js
@@ -5,13 +5,12 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import document from 'global/document';
+import env from 'min-react-env';
 import createYouTube from './createYouTube';
 
-global.window = { document };
-global.document = document;
+Object.assign(global, env);
 
-const render = (props) => {
+const render = (initialProps) => {
   const { YouTube, sdkMock, playerMock } = createYouTube();
 
   let component;
@@ -20,27 +19,29 @@ const render = (props) => {
     constructor(ytProps) {
       super(ytProps);
 
-      this.state = ytProps;
+      this.state = { props: ytProps };
     }
 
     render() {
+      const { props } = this.state;
+
       return (
         <YouTube
           ref={(youtube) => { component = youtube; }}
-          {...this.state}
+          {...props}
         />
       );
     }
   }
 
-  const div = document.createElement('div');
+  const div = env.document.createElement('div');
   const container = new Promise((resolve) => {
-    ReactDOM.render(<Container {...props} ref={resolve} />, div);
+    ReactDOM.render(<Container {...initialProps} ref={resolve} />, div);
   });
 
   function rerender(newProps) {
     return container.then(wrapper => new Promise((resolve) => {
-      wrapper.setState(newProps, () => {
+      wrapper.setState({ props: newProps }, () => {
         Promise.resolve().then(resolve);
       });
     }));
