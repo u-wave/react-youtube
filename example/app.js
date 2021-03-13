4c496d9ea3c99b3975c4ca6db8a6b043338eb7c9
Renée Kooi <renee@kooi.me>
2021-03-13 16:15:07 +0100

    example: use React Hooks

--- example/app.js
+++ example/app.js
@@ -3,6 +3,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import YouTube from '..';
 
+const {
+  useState,
+} = React;
+
 const videos = [
   { id: 'ZuuVjuLNvFY', name: 'JUNNY - kontra (Feat. Lil Gimch, Keeflow)' },
   { id: 'PYE7jXNjFWw', name: 'T W L V - Follow' },
@@ -19,131 +23,108 @@ const defaultVideo = hashVideoRx.test(hash)
   ? parseInt(hash.replace(hashVideoRx, '$1'), 10)
   : 0;
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+function App() {
+  const [videoIndex, setVideoIndex] = useState(defaultVideo);
+  const [suggestedQuality, setSuggestedQuality] = useState('auto');
+  const [volume, setVolume] = useState(1);
+  const [paused, setPaused] = useState(false);
 
-    this.state = {
-      videoIndex: defaultVideo,
-      suggestedQuality: 'auto',
-      volume: 1,
-      paused: false,
-    };
+  const video = videos[videoIndex];
 
-    this.handlePause = this.handlePause.bind(this);
-    this.handlePlayerPause = this.handlePlayerPause.bind(this);
-    this.handlePlayerPlay = this.handlePlayerPlay.bind(this);
-    this.handleVolume = this.handleVolume.bind(this);
-    this.handleQuality = this.handleQuality.bind(this);
+  function selectVideo(index) {
+    setVideoIndex(index);
   }
 
-  selectVideo(index) {
-    this.setState({ videoIndex: index });
+  function handlePause(event) {
+    setPaused(event.target.checked);
   }
 
-  handlePause(event) {
-    this.setState({
-      paused: event.target.checked,
-    });
+  function handlePlayerPause() {
+    setPaused(true);
   }
 
-  handlePlayerPause() {
-    this.setState({ paused: true });
+  function handlePlayerPlay() {
+    setPaused(false);
   }
 
-  handlePlayerPlay() {
-    this.setState({ paused: false });
+  function handleVolume(event) {
+    setVolume(parseFloat(event.target.value));
   }
 
-  handleVolume(event) {
-    this.setState({
-      volume: parseFloat(event.target.value),
-    });
+  function handleQuality(event) {
+    setSuggestedQuality(qualities[event.target.selectedIndex]);
   }
 
-  handleQuality(event) {
-    this.setState({
-      suggestedQuality: qualities[event.target.selectedIndex],
-    });
-  }
-
-  render() {
-    const {
-      videoIndex, volume, paused, suggestedQuality,
-    } = this.state;
-
-    const video = videos[videoIndex];
-    return (
-      <div className="row">
-        <div className="col s4">
-          <h5>
-            Video
-          </h5>
-          <div className="collection">
-            {videos.map((choice, index) => (
-              <a
-                key={choice.id}
-                href={`#!/video/${index}`}
-                className={`collection-item ${video === choice ? 'active' : ''}`}
-                onClick={() => this.selectVideo(index)}
-              >
-                {choice.name}
-              </a>
-            ))}
-          </div>
-          <h5>
-            Paused
-          </h5>
-          <p>
-            <label htmlFor="paused">
-              <input
-                type="checkbox"
-                id="paused"
-                checked={paused}
-                onChange={this.handlePause}
-              />
-              <span>Paused</span>
-            </label>
-          </p>
-          <h5>
-            Volume
-          </h5>
-          <input
-            type="range"
-            value={volume}
-            min={0}
-            max={1}
-            step={0.01}
-            onChange={this.handleVolume}
-          />
-          <h5>
-            Quality
-          </h5>
-          <select className="browser-default" onChange={this.handleQuality}>
-            {qualities.map((quality) => (
-              <option key={quality} value={quality}>
-                {quality}
-              </option>
-            ))}
-          </select>
+  return (
+    <div className="row">
+      <div className="col s4">
+        <h5>
+          Video
+        </h5>
+        <div className="collection">
+          {videos.map((choice, index) => (
+            <a
+              key={choice.id}
+              href={`#!/video/${index}`}
+              className={`collection-item ${video === choice ? 'active' : ''}`}
+              onClick={() => selectVideo(index)}
+            >
+              {choice.name}
+            </a>
+          ))}
         </div>
-        <div className="col s8 center-align">
-          <YouTube
-            video={video.id}
-            width={640}
-            height={480}
-            autoplay
-            controls={false}
-            suggestedQuality={suggestedQuality}
-            volume={volume}
-            paused={paused}
-            onPause={this.handlePlayerPause}
-            onPlaying={this.handlePlayerPlay}
-          />
-        </div>
+        <h5>
+          Paused
+        </h5>
+        <p>
+          <label htmlFor="paused">
+            <input
+              type="checkbox"
+              id="paused"
+              checked={paused}
+              onChange={handlePause}
+            />
+            <span>Paused</span>
+          </label>
+        </p>
+        <h5>
+          Volume
+        </h5>
+        <input
+          type="range"
+          value={volume}
+          min={0}
+          max={1}
+          step={0.01}
+          onChange={handleVolume}
+        />
+        <h5>
+          Quality
+        </h5>
+        <select className="browser-default" onChange={handleQuality}>
+          {qualities.map((quality) => (
+            <option key={quality} value={quality}>
+              {quality}
+            </option>
+          ))}
+        </select>
       </div>
-    );
-  }
+      <div className="col s8 center-align">
+        <YouTube
+          video={video.id}
+          width={640}
+          height={480}
+          autoplay
+          controls={false}
+          suggestedQuality={suggestedQuality}
+          volume={volume}
+          paused={paused}
+          onPause={handlePlayerPause}
+          onPlaying={handlePlayerPlay}
+        />
+      </div>
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById('example'));
