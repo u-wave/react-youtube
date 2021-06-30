fe06529659a938bab245c11449853658d43ffd53
Renée Kooi <renee@kooi.me>
2021-06-30 13:09:23 +0200

    lint

--- rollup.config.js
+++ rollup.config.js
@@ -5,8 +5,17 @@ const meta = require('./package.json');
 export default {
   input: './src/index.js',
   output: [
-    { format: 'cjs', file: meta.exports['.'].require, exports: 'named', sourcemap: true },
-    { format: 'esm', file: meta.exports['.'].import, sourcemap: true },
+    {
+      format: 'cjs',
+      file: meta.exports['.'].require,
+      exports: 'named',
+      sourcemap: true,
+    },
+    {
+      format: 'esm',
+      file: meta.exports['.'].import,
+      sourcemap: true,
+    },
   ],
 
   external: Object.keys(meta.dependencies)
