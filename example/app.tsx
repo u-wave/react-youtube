73a9120a61eef11a5f61e5a14390103578cd7c52
Renée Kooi <renee@kooi.me>
2022-12-13 15:46:46 +0100

    example: use vite

--- example/app.tsx
+++ example/app.tsx
@@ -1,11 +1,145 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { styled } from '@stitches/react';
 import YouTube from '@u-wave/react-youtube';
 
 const {
   useCallback,
   useState,
 } = React;
+
+const Link = styled('a', {
+  color: '#c72e6c',
+  textDecoration: 'underline',
+  '&:visited': { color: '#c72e6c' },
+});
+const Label = styled('label', {
+  cursor: 'pointer',
+  fontSize: '1rem',
+  '& input': { marginRight: '.5rem' },
+});
+const H1 = styled('h1', {
+  fontWeight: 400,
+  fontSize: '3rem',
+});
+const H5 = styled('h5', {
+  margin: '1.0933333333rem 0 .656rem 0',
+  fontWeight: 400,
+  fontSize: '1.64rem',
+  lineHeight: '110%',
+});
+const Input = styled('input', {
+  '&[type="range"]': {
+    background: 'transparent',
+    appearance: 'none',
+    cursor: 'pointer',
+    margin: '1rem 0',
+    padding: 0,
+    outline: 'none',
+    width: '16rem',
+    accentColor: '#9d2053',
+
+    '&::-moz-range-track, &::-webkit-slider-runnable-track': {
+      height: 2,
+      backgroundColor: 'rgba(255, 255, 255, 0.38)',
+    },
+    '&::-moz-range-thumb, &::-webkit-slider-thumb': {
+      border: 'none',
+      width: 14,
+      height: 14,
+      background: '#9d2053',
+      marginTop: -5,
+      borderRadius: 9999,
+    },
+  },
+});
+
+const Nav = styled('nav', {
+  backgroundColor: '#9d2053',
+  color: 'white',
+  padding: '0 2rem',
+  display: 'flex',
+  alignItems: 'center',
+  height: '64px',
+  gap: '2rem',
+});
+const NavButton = styled('a', {
+  color: 'white',
+  padding: '.75rem',
+  borderRadius: '4px',
+  textTransform: 'uppercase',
+  textDecoration: 'none',
+  fontWeight: 500,
+  fontSize: '0.875rem',
+  lineHeight: '1.75',
+  '&:hover': {
+    backgroundColor: 'rgba(255, 255, 255, 0.08)',
+  },
+});
+
+const LogoLink = styled('a', {
+  height: '48px',
+  lineHeight: '48px',
+  '& img': { height: 48 },
+});
+
+const List = styled('ul', {
+  listStyle: 'none',
+  margin: 0,
+  padding: 0,
+  display: 'flex',
+  variants: {
+    direction: {
+      vertical: { flexDirection: 'column' },
+      horizontal: { flexDirection: 'row' },
+    },
+  },
+  defaultVariants: {
+    direction: 'vertical',
+  },
+});
+const ListItem = styled('li', {
+  margin: 0,
+  padding: '0 1rem',
+  fontSize: '1rem',
+  color: 'white',
+  height: '48px',
+  display: 'flex',
+  alignItems: 'center',
+  textDecoration: 'none',
+  '&:hover': {
+    backgroundColor: 'rgba(255, 255, 255, 0.08)',
+  },
+  variants: {
+    active: {
+      true: {
+        backgroundColor: 'rgba(48, 48, 54, 0.3)',
+      },
+    },
+  },
+});
+
+const Layout = styled('main', {
+  display: 'grid',
+  gridTemplateAreas: '"header header" "options embed"',
+  gridTemplateColumns: '24rem auto',
+  gridGap: '1rem',
+  margin: 'auto',
+  width: 'min-content',
+  '@media (max-width: 800px)': {
+    gridTemplateAreas: '"header" "embed" "options"',
+    gridTemplateColumns: 'auto',
+  },
+});
+const Header = styled('div', {
+  gridArea: 'header',
+});
+const Options = styled('div', {
+  gridArea: 'options',
+});
+const Embed = styled('div', {
+  gridArea: 'embed',
+});
 
 const videos = [
   { id: 'ZuuVjuLNvFY', name: 'JUNNY - kontra (Feat. Lil Gimch, Keeflow)' },
@@ -22,7 +156,7 @@ function getInitialVideo() {
     : 0;
 }
 
-function App() {
+function Example() {
   const [videoIndex, setVideoIndex] = useState(getInitialVideo);
   const [volume, setVolume] = useState(1);
   const [paused, setPaused] = useState(false);
@@ -46,41 +180,36 @@ function App() {
   }, []);
 
   return (
-    <div className="row">
-      <div className="col s4">
-        <h5>
-          Video
-        </h5>
-        <div className="collection">
+    <>
+      <Options>
+        <H5>Video</H5>
+        <List as="div">
           {videos.map((choice, index) => (
-            <a
+            <ListItem
+              as="a"
               key={choice.id}
               href={`#!/video/${index}`}
-              className={`collection-item ${video === choice ? 'active' : ''}`}
               onClick={() => setVideoIndex(index)}
+              active={videoIndex === index}
             >
               {choice.name}
-            </a>
+            </ListItem>
           ))}
-        </div>
-        <h5>
-          Paused
-        </h5>
+        </List>
+        <H5>Paused</H5>
         <p>
-          <label htmlFor="paused">
-            <input
+          <Label htmlFor="paused">
+            <Input
               type="checkbox"
               id="paused"
               checked={paused}
               onChange={handlePause}
             />
             <span>Paused</span>
-          </label>
+          </Label>
         </p>
-        <h5>
-          Volume
-        </h5>
-        <input
+        <H5>Volume</H5>
+        <Input
           type="range"
           value={volume}
           min={0}
@@ -88,8 +217,8 @@ function App() {
           step={0.01}
           onChange={handleVolume}
         />
-      </div>
-      <div className="col s8 center-align">
+      </Options>
+      <Embed>
         <YouTube
           video={video.id}
           width={640}
@@ -101,8 +230,36 @@ function App() {
           onPause={handlePlayerPause}
           onPlaying={handlePlayerPlay}
         />
-      </div>
-    </div>
+      </Embed>
+    </>
+  );
+}
+
+function App() {
+  return(
+    <>
+      <Nav>
+        <LogoLink href="https://u-wave.net">
+          <img src="https://u-wave.net/images/logo-white.png" alt="" />
+        </LogoLink>
+        <List direction="horizontal">
+          <li><NavButton href="https://hub.u-wave.net/">Join</NavButton></li>
+          <li><NavButton href="https://u-wave.net/install">Install</NavButton></li>
+          <li><NavButton href="https://u-wave.net/react-youtube">react-youtube</NavButton></li>
+        </List>
+      </Nav>
+      <Layout>
+        <Header>
+          <H1>@u-wave/react-youtube example</H1>
+          <p>
+            An example YouTube player using <Link href="https://facebook.github.io/react">React</Link>
+            {' '}and <Link href="https://github.com/u-wave/react-youtube">@u-wave/react-youtube</Link>.
+            {' '}<Link href="https://github.com/u-wave/react-youtube/tree/default/example">view source</Link>
+          </p>
+        </Header>
+        <Example />
+      </Layout>
+    </>
   );
 }
 
